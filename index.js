02a37e6df9f7d45e01f673d3a287e3cc20f6c79a
shourovahmd <moh.abuhanifa@gmail.com>
2023-08-12 03:24:16 +0600

    dotenv added

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
-const {
-  ApolloServerPluginDrainHttpServer,
-} = require("@apollo/server/plugin/drainHttpServer");
 const express = require("express");
-const http = require("http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { typeDefs, resolvers } = require("./schema");
+require("dotenv").config();
+const port = process.env.PORT || 4000;
+
+
 
 const app = express();
-
-const httpServer = http.createServer(app);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
 async function main() {
   await server.start();
-  app.use(
-    "/",
-    cors(),
-    bodyParser.json(),
-    expressMiddleware(server, {
-      context: async ({ req }) => ({ token: req.headers.token }),
-    })
-  );
-
-  httpServer.listen({ port: 4000 }, () => {
-    console.log(`🚀 Server ready at http://localhost:4000/`);
-  });
+  app.use("/graphql", expressMiddleware(server));
 }
 
 main().catch((error) => {
   console.error("Error starting the server:", error);
+});
+
+app.listen(4000, () => {
+  console.log(` Server ready at http://localhost:4000/graphql`);
 });
